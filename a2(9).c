c59b197e79655cb4d717cb798d8675cc411d1e76
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-13 01:18:55 +0530

    Add files via upload

---
+++ a2(9).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a;
+float b;
+char c;
+double d;
+printf("\nSize of int : %d byte",sizeof(a));
+printf("\nSize of float : %d byte",sizeof(b));
+printf("\nSize of char : %d byte",sizeof(c));
+printf("\nSize of double : %d byte",sizeof(d));
+return 0;
+}
